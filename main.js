6a2f7acd648766ed9af6c2df898ec1907949f87d
Istavya <104734572+Istavya@users.noreply.github.com>
2023-11-25 13:41:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+song = "";
+leftwristX = 0;
+leftwristY = 0;
+rightWristX = 0;
+rightwristY = 0;
+
+function preload()
+{
+  song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO)
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX+ "leftwristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX+"rightWristY = "+rightWristY);
+    }
+}
+
